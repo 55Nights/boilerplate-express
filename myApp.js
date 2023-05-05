944843386aad397476c7c6a0c58a7ba69e537bf7
55Nights <1905587@students.kca.ac.ke>
2023-05-05 15:38:11 +0300

    Example of an express api

--- myApp.js
+++ myApp.js
@@ -1,33 +1,71 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
+let bodyparser = require('body-parser')
+
+// create an express instance
 let app = express();
 
+//log to the console
+console.log("Hello World");
 
-console.log("Hello World")
-
-
-
-
-
-
+//display static files
+//app.use() displays to all routes
+app.use("/public", express.static(__dirname + "/public"));
 
 
+// using app.use to implemment all routes
+app.use(function (req, res, next) {
+  var s = req.method + " " + req.path + " - " + req.ip;
+  console.log(s);
+  next();
+});
 
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json());
 
+// /root route
+app.get("/", function (req, res) {
+  var absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+// return response as json using .json()
+app.get("/json", function (req, res) {
+  var obj = { message: "Hello json" };
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    obj.message = obj["message"].uppercase;
+  }
+  res.json(obj);
+});
 
+app.get("/now",  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    let t = { time: req.time };
+    console.log(t);
+    res.json(t);
+  }
+);
 
+app.get("/:word/echo", function (req, res) {
+  var word = req.params.word;
+  word = { echo: word };
+  res.json(word);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+app.route("/name").get(function (req, res) {
+    let firstname = req.query.first
+    let lastname = req.query.last
+    let fullname = firstname + " " + lastname
+    let obj = {"name": fullname}
+    res.json(obj)
+  }).post(function (req, res) {  
+    let firstname = req.body.first
+    let lastname = req.body.last
+    let fullname = firstname + " " + lastname
+    let obj = {"name": fullname}
+    res.json(obj)
+  })
+module.exports = app;
